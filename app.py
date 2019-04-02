f96b1dde64671417c5ea88d9d9993a219fe52e49
Johnson Li <jli39@stuy.edu>
2019-04-01 20:19:54 -0400

    flask skeleton

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from urllib import request
+
+from flask import Flask, request, render_template, \
+                  flash, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+
+app.run()
