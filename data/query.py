292719ecf91f8f9084f3cc2cb7f13eceffa70baa
Johnson Li <jli39@stuy.edu>
2019-04-04 15:23:40 -0400

    finished parser and converted some data to json

--- data/query.py
+++ data/query.py
@@ -1,24 +1,20 @@
 import os
-import json
+import json, csv
 
-import pandas
+# import pandas
 
 def col_query(file_name, *args):
-    try:
-        data = []
-        csvdata = pandas.read_csv(file_name)
+    data = []
+    with open(file_name) as file:
+        reader = csv.reader(file)
+        col_titles = next(reader)
 
-        # for arg in args:
-        #     if str(arg) in list(csvdata):
-        #         data[str(arg)] = json.dumps(csvdata[str(arg)].tolist())
-        for i in range(1, csvdata.size):
+        for row in reader:
             to_add = {}
-            print(csvdata.values[i])
+            for i in range(len(col_titles)):
+                to_add[col_titles[i]] = row[i]
+            data.append(to_add)
 
-        return data
+        return json.dumps(data)
 
-    except Exception as e:
-        print(e)
-
-res = col_query('openAQ.csv', 'location', 'city', 'value', 'latitude', 'longitude')
-print(res)
+print(col_query('USCS_OverviewMap.csv'))
