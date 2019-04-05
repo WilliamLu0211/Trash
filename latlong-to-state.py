7e1ea3c0521d2ab15b347dd23a2597c551173e0b
ivzhang1 <izhang1@stuy.edu>
2019-04-05 12:55:37 -0400

    change state abbreviations to full state names

--- latlong-to-state.py
+++ latlong-to-state.py
@@ -1,4 +1,4 @@
-import urllib.request
+#import urllib.request
 import json
 import ssl
 
@@ -41,5 +41,86 @@ def get_state():
 
     #
     # print(jason)
+def abbrev_to_full():
+    states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'Puerto Rico': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming',
+        'District of Columbia': 'Maryland'
+    }
+    f = open("data/openAQ.csv", "r")
+    lines = f.readlines()
+    res = lines[0]
+    lines = lines[1:]
 
-get_state()
+    for line in lines:
+        line = line.split(",")
+        if(line[1]) != "Sonora" and (line[1]) != "Chihuahua":
+            line[1] = states[line[1]]
+
+            res += ",".join(line)
+
+
+
+    f.close()
+    g = open("data/openAQ.csv", "w")
+    g.write(res)
+    g.close()
+    print("done")
+
+abbrev_to_full()
+#get_state()
