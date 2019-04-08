a6fa03b7e3093cbd9413acc56f0f97d907bd3cb5
ivzhang1 <izhang1@stuy.edu>
2019-04-07 20:38:31 -0400

    add air pollution colors

--- v1/setup.js
+++ v1/setup.js
@@ -53,7 +53,7 @@ var car_accidents = {'45': 1439, '28': 929, '50': 79, '53': 795, '32': 476, '04'
 
 var lung_cancer_state = {'56': 50, '54': 110, '37': 80, '22': 76, '45': 82, '36': 70, '53': 62, '42': 84, '02': 47, '25': 75, '23': 101, '27': 63, '20': 69, '21': 110, '04': 59, '49': 22, '46': 69, '47': 90, '08': 44, '09': 74, '28': 86, '29': 89, '40': 81, '41': 68, '05': 95, '24': 63, '44': 86, '01': 82, '26': 80, '12': 84, '06': 44, '13': 65, '10': 89, '39': 85, '38': 67, '15': 57, '48': 50, '17': 73, '16': 56, '19': 78, '32': 61, '31': 67, '30': 72, '51': 65, '50': 83, '35': 47, '34': 67, '55': 72, '18': 83, '33': 82} //NUMBER OF CASES PER 100,000 people
 
-var air_pollution = {'56': 29.0, '54': 5.7, '28': 39.0, '22': 13.9, '50': 4.2, '19': 17.0, '35': 29.0, '23': 60.0, '24': 26.0, '25': 43.0, '26': 51.5, '27': 59.0, '06': 233.0, '21': 28.0, '04': 79.0, '49': 91.0, '46': 130.0, '47': 9.6, '08': 24.0, '45': 31.0, '42': 29.7, '29': 66.0, '40': 43.0, '41': 14.0, '09': 12.0, '05': 6.0, '51': 9.6, '02': 76.0, '01': 25.0, '13': 12.0, '12': 38.0, '20': 40.0, '10': 4.4, '39': 19.2, '38': 32.0, '15': 29.0, '48': 29.4, '17': 17.5, '16': 20.0, '55': 28.0, '32': 74.0, '31': 7.0, '30': 38.0, '37': 22.0, '36': 87.0, '53': 151.0, '34': 25.0, '33': 5.0, '18': 22.0, '44': 3.0} //Measured in µg/m³
+var air_pollution = {'56': 29.0, '54': 5.7, '28': 39.0, '22': 13.9, '50': 4.2, '19': 17.0, '35': 29.0, '23': 60.0, '24': 26.0, '25': 43.0, '26': 51.5, '27': 59.0, '06': 233.0, '21': 28.0, '04': 79.0, '49': 91.0, '46': 130.0, '47': 9.6, '08': 24.0, '45': 31.0, '42': 29.7, '29': 66.0, '40': 43.0, '41': 14.0, '09': 12.0, '05': 6.0, '51': 9.6, '02': 76.0, '01': 25.0, '13': 12.0, '12': 38.0, '20': 40.0, '10': 4.4, '39': 19.2, '38': 32.0, '15': 29.0, '48': 29.4, '17': 17.5, '16': 20.0, '55': 28.0, '32': 74.0, '31': 7.0, '30': 38.0, '37': 22.0, '36': 87.0, '53': 151.0, '34': 25.0, '33': 5.0, '18': 22.0, '44': 3.0} //Measured in  µg/m³
 
 
 var width = 960, height = 650, centered;
@@ -81,6 +81,11 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
    .selectAll("path")
    .data(topojson.feature(us, us.objects.states).features)
    .enter().append("path")
+   .style('fill', (d, i) => {
+                    var pollute = Math.round(Math.floor(256 * air_pollution[d.id] / (233.0 + 1)))
+                    console.log(air_pollution[d.id], states[d.id])
+                    return rgbToHex(255 - pollute);
+                })
    .attr("d", path)
    .on("click", clicked)
    .on("mouseover", hovered)
@@ -90,7 +95,25 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
    .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
    .attr("id", "state-borders")
    .attr("d", path);
+
+
 });
+
+function rgbToHex(c) {
+    if(c == 255){
+      c = 250
+    }
+
+    var hex = c.toString(16);
+    var component = "";
+    if(hex.length == 1){
+      component = "0" + hex;
+    }
+    else{
+      component = hex;
+    }
+    return "#" + component + component + component;
+}
 
 function clicked(d) {
   if(text)
@@ -154,6 +177,10 @@ var unhovered = function(d){
   text = null;
 };
 
+var car_density = function(d){
+  // d.attr('fill', function(d) { return color(d); })
+}
+
 
 var cd = document.getElementById("cd");
 var lcr = document.getElementById("lcr");
@@ -161,6 +188,7 @@ var lcr = document.getElementById("lcr");
 cd.addEventListener('click', function(){
   cd.setAttribute("class", "btn btn-primary");
   lcr.setAttribute("class", "btn btn-secondary");
+  car_density();
 });
 
 lcr.addEventListener('click', function(){
